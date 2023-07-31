421c7ae84d664218bb1a3818930acf2015da2309
SvetlanaDanilova <90624619+SvetlanaDanilova@users.noreply.github.com>
2023-07-31 23:20:16 +0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -259,6 +259,11 @@ def main():
 
     st.title('Классификация отзывов')
 
+    tokenizer = WordPunctTokenizer()
+
+    with st.spinner('Скачивание предобученной эмбеддинговой модели'):
+        gensim_embedding_model = api.load('glove-twitter-200')
+
     if os.path.exists('model_label.pth') and os.path.exists('model_rating.pth'):
         st.header('Загрузка моделей')
         model_label = torch.load('model_label.pth')
@@ -272,12 +277,10 @@ def main():
             train_data, train_label, train_rating, test_data, test_label, test_rating = load_data()
     
         with st.spinner('Токенизация'):
-            tokenizer = WordPunctTokenizer()
             texts_train = tokenize(tokenizer, train_data)
             texts_test = tokenize(tokenizer, test_data)
     
         with st.spinner('Создание эмбеддингов'):
-            gensim_embedding_model = api.load('glove-twitter-200')
             X_train_emb = [text_to_average_embedding(text, tokenizer, gensim_embedding_model) for text in texts_train]
             X_test_emb = [text_to_average_embedding(text, tokenizer, gensim_embedding_model) for text in texts_test]
     
